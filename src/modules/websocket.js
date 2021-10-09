6cf2e4a144864a103b92c2f44df80ccfe8da4099
Thomas Luca <thomasluca008@gmail.com>
2021-10-09 16:50:25 +0200

    fix: prevent sending message over ws when no connection to ws client

--- src/modules/websocket.js
+++ src/modules/websocket.js
@@ -5,15 +5,21 @@ class WS {
     constructor(server) {
         this.ws = new WebSocket.Server({server})
         this.client = null
+        this.isConnected = false
         this.ws.on('connection', (client) => {
             console.log('WebSocket connection...')
             this.client = client
+            this.isConnected = true
             this.webSocketSend({ message: "welcome", value: "Welcome using WebSocket"})
         })
     }
 
     webSocketSend(data){
-        this.client.send(JSON.stringify(data))
+        if (this.isConnected) {
+            this.client.send(JSON.stringify(data))
+        } else {
+            console.log("Unable to send to client: Client not connected.")
+        }
     }
 }
 

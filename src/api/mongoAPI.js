4ddb10b375f1d17bb30d02f221fb1c36b4ca11c9
Aaron <aaron.van.vyve@telenet.be>
2021-10-15 12:28:47 +0200

    preparing to change mongodb

--- src/api/mongoAPI.js
+++ src/api/mongoAPI.js
@@ -6,13 +6,13 @@ let client;
 class MongoAPI{
     
     constructor(){
-        this.url ="mongodb+srv://AaronVanV:M0ng0dAar0n@aaronvanv.ocujk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-        this.dbName= "Project"
-        this.collection="accounts"
-        this.collection2="devices"
+        this.url =`${process.env.MONGO_API_CONNECTION_TOKEN}`
+        this.dbName= `${process.env.MONGO_DATABASE}`
+        this.users=`${process.env.MONGO_USERSCOLLECTION}`
+        this.devices=`${process.env.MONGO_DEVICESCOLLECTION}`
         this.client= new MongoClient(this.url)
-        this.mongo=this.client.db(this.dbName).collection(this.collection)
-        this.mongo2=this.client.db(this.dbName).collection(this.collection2)
+        this.mongoUsers=this.client.db(this.dbName).collection(this.users)
+        this.mongoDevices=this.client.db(this.dbName).collection(this.devices)
         
     }
     async connector(){
@@ -27,36 +27,36 @@ class MongoAPI{
     async findAllUsers(){
 
         await this.connector()
-        return this.mongo.find({}).toArray()
+        return this.mongoUsers.find({}).toArray()
     }
 
     async findUserByName(username,password){
 
         await this.connector()
-        return  this.mongo.findOne({$and:[{username:username},{password:password}]});
+        return  this.mongoUsers.findOne({$and:[{username:username},{password:password}]});
     }
 
     async createUser(username,password){
         await this.connector()
-        const lol = await this.mongo.findOne({username:username});
+        const lol = await this.mongoUsers.findOne({username:username});
         console.log(lol)
         if(lol){
             return Promise.resolve("Already exists")
         }else{
-            return this.mongo.insertOne({username:username,password:password})  
+            return this.mongoUsers.insertOne({username:username,password:password})  
         }
     }
 
     async changePassword(username,password,newPassword){
         
         await this.connector()
-        return this.mongo.updateOne({$and:[{username:username},{password:password}]},{$set: {password:newPassword}})
+        return this.mongoUsers.updateOne({$and:[{username:username},{password:password}]},{$set: {password:newPassword}})
             
     }
 
     async deleteUser(username,password){
         await this.connector()
-        return this.mongo.deleteOne({$and:[{username:username},{password:password}]})
+        return this.mongoUsers.deleteOne({$and:[{username:username},{password:password}]})
     }
 
 
@@ -66,14 +66,14 @@ class MongoAPI{
 
         await this.connector()
         
-        return this.mongo2.find({}).toArray()
+        return this.mongoDevices.find({}).toArray()
 
         
     }
 
     async createDevice(name,location){
         await this.connector()
-        return this.mongo2.insertOne({name:name,location:location})
+        return this.mongoDevices.insertOne({name:name,location:location})
     }
 
 

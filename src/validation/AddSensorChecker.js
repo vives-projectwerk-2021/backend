3455dfc3f37f8d0ba7aa1e4f317bbb6fbe6a24a3
Falk Felix <felix.falk2903@gmail.com>
2021-11-05 10:45:07 +0100

    fixed a small bug

--- src/validation/AddSensorChecker.js
+++ src/validation/AddSensorChecker.js
@@ -9,7 +9,7 @@ const AddSensorChecker = {
             "firstname": {"type": "string"},
             "lastname": {"type":"string"}
         },
-        "required": ["deviceid","devicename", "location","firstname","lastname"],
+        "required": ["devicename", "location","firstname","lastname"],
         "additionalProperties": false
         //first and last name are at the moment given with the addsensor but should not be necessery later when you have to loging to add sensors
     }

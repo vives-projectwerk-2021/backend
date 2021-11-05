488a32f54162644742a9ee8672081e83313a266f
Thomas Luca <thomasluca008@gmail.com>
2021-11-05 11:57:47 +0100

    websocket now communicates with multiple clients

--- src/modules/websocket.js
+++ src/modules/websocket.js
@@ -1,27 +1,29 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 
 class WSS {
     constructor(server) {
         const wss = new WebSocketServer({server});
-        this.client = null;
-        this.isConnected = false;
+        this.clients = [];
         this.checkForConnection(wss);
     }
 
     checkForConnection(wss){
         wss.on('connection', (client) => {
             console.log('WebSocket connection...')
-            this.client = client
-            this.isConnected = true
-            this.webSocketSend({ message: "welcome", value: "Welcome using WebSocket"})
+            this.clients.push(client)
+            client.send(JSON.stringify({ message: "welcome", value: "Welcome using WebSocket"}))
         })
     }
 
     webSocketSend(data){
-        if (this.isConnected) {
-            this.client.send(JSON.stringify(data))
+        if (this.clients.length > 0) {
+            this.clients.forEach( (client) => {
+                if (client.readyState === WebSocket.OPEN){
+                    client.send(JSON.stringify(data));
+                }
+            });
         } else {
-            console.log("Unable to send to client: Client not connected.")
+            console.log("Unable to send to clients: No clients connected.")
         }
     }
 }

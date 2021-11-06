809ea0fb3bbc682b3a1e14290172a6a1d1c53e9b
Thomas Luca <thomasluca008@gmail.com>
2021-11-06 12:40:49 +0100

    changed route name for live-data

--- src/server.js
+++ src/server.js
@@ -13,22 +13,23 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 const server = http.createServer(app)
-const wss = new WSS(server)
 
-const posts = []
+let resentLiveData = {}
+const wss = new WSS(server, resentLiveData)
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-  console.log("connected")
+  res.send(`<h1>Connected to Pulu Backend</h1>
+            <p> Go to /live-datas to see most recent device data</p>`)
 })
 
-app.get('/posts', (req, res) => {
-  res.status(201).send(posts)
+app.get('/live-data', (req, res) => {
+  res.status(201).send(resentLiveData)
 })
 
 
-//Data coming from our devices in the field is send here to the frondend
-app.post('/posts', (req, res) => {
+//Data coming from our devices in the field is send here to the frontend
+app.post('/live-data', (req, res) => {
   const data = req.body
   const validation = validate(data, DeviceDataChecker.create)
   if (!validation.valid) {
@@ -39,9 +40,9 @@ app.post('/posts', (req, res) => {
     })
     return;
   }
-  posts.push(data)
+  resentLiveData = data
   wss.webSocketSend(data)
-  res.status(201).json(posts)
+  res.status(201).json(resentLiveData)
 })
 
 // INFLUX

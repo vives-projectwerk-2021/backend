9b0c6efd0949a083a7ee040ac4c92badcf797dc8
Noah Debaere <noah.debaere@gmail.com>
2021-11-20 13:36:51 +0100

    Added the influx:8086

--- src/databases/values_db.js
+++ src/databases/values_db.js
@@ -12,7 +12,7 @@ class values_db {
 
     async connector() {
         console.log("Connecting to the influxDB: " + this.org);
-        this.client = new InfluxDB({url: 'https://proxy-38239.trikthom.com', token: this.token});
+        this.client = new InfluxDB({url: 'influx:8086', token: this.token});
         this.queryApi = this.client.getQueryApi(this.org);
     }
 

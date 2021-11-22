941d8dc7b560ba8c848cfb791fbb07b772430f6d
Noah Debaere <noah.debaere@gmail.com>
2021-11-22 13:10:21 +0100

    Added new way of adding datapoints to the influxDB

--- src/databases/values_db.js
+++ src/databases/values_db.js
@@ -21,22 +21,66 @@ class values_db {
         const writeApi = this.client.getWriteApi(this.org, this.bucket)
         writeApi.useDefaultTags({host: data.data.device_id })
 
-        // Writing the light values
-       /*  writeApi.writePoint(new Point('sensors')
-        .stringField('type', "Light")
-        .intField('status', data.data.sensors.light.status)
-        .intField('light', data.data.sensors.light.value)) */
+        // Array to save datapoints to
+        let points = []
+
+        // Writing the general data
+        points.push(Point("general")
+        .stringField("device_id", data.data.device_id)
+        .floatField("count", data.data.count))
 
         // Writing the voltage values
-        /* writeApi.writePoint(new Point('sensors')
-        .stringField('type', "Voltage")
-        .stringField('part', "Battery")
-        .intField('status', data.data.sensors.voltage.battery.status)
-        .intField('value', data.data.sensors.voltage.battery.value)) */
+        points.push(Point('sensors')
+        .tag("voltage", "battery")
+        .floatField("status", data.data.sensors.voltage.battery.status)
+        .floatField("value", data.data.sensors.voltage.battery.value))
+
+        // Writing the light values
+        points.push(Point('sensors')
+        .tag("type", "light")
+        .floatField("status", data.data.sensors.light.status)
+        .floatField("value", data.data.sensors.light.value))
+
+        // Writing the moisture levels
+        points.push(Point('sensors')
+        .tag("moisture", "level1")
+        .floatField("status", data.data.sensors.moisture.level1.status)
+        .floatField("value", data.data.sensors.moisture.level1.value))
+
+        points.push(Point('sensors')
+        .tag("moisture", "level2")
+        .floatField("status", data.data.sensors.moisture.level2.status)
+        .floatField("value", data.data.sensors.moisture.level2.value))
+
+        points.push(Point('sensors')
+        .tag("moisture", "level3")
+        .floatField("status", data.data.sensors.moisture.level3.status)
+        .floatField("value", data.data.sensors.moisture.level3.value))
+
+        points.push(Point('sensors')
+        .tag("moisture", "level4")
+        .floatField("status", data.data.sensors.moisture.level4.status)
+        .floatField("value", data.data.sensors.moisture.level4.value))
 
         // Writing the temperature values
-        writeApi.writePoint(new Point('temp')
-        .floatField('air', data.data.sensors.temperature.air.value))
+        points.push(Point('sensors')
+        .tag("temperature", "air")
+        .floatField("status", data.data.sensors.temperature.air.status)
+        .floatField("value", data.data.sensors.temperature.air.value))
+
+        points.push(Point("sensors")
+        .tag("temperature", "ground")
+        .floatField("status", data.data.sensors.temperature.ground.status)
+        .floatField("value", data.data.sensors.temperature.ground.value))
+
+        // Writing the meta data
+        points.push(Point("meta")
+        .floatField("gateway_cnt", data.data.meta.gateway_cnt)
+        .floatField("strongest_rssi", data.data.meta.strongest_rssi)
+        .stringField("strongest_gateway", data.data.sensors.strongest_gateway))
+
+        // Writing the points to the DB and checking for errors
+        writeApi.writePoints(points)
         writeApi
             .close()
             .then(() => {

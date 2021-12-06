bb82f66a1690b12b5d6993b90b7eb330a6537f99
Thomas Luca <thomasluca008@gmail.com>
2021-12-06 18:14:01 +0100

    made request to ttn-device-manager for serial connection

---
+++ src/api/ttn.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import config from '../config/config.js'
+
+const api = axios.create({
+  baseURL: config.ttn_device_manager_base_url
+});
+
+const TTN = {    
+  registerDevice(json){
+    return api.post("/devices", json);
+  }
+}
+
+export { api, TTN }
